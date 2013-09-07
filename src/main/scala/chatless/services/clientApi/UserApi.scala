f5bf68425e7e64a262c95d23b909812c3f88ba26
aidan coyne <raptros.v76@gmail.com>
2013-09-07 16:15:50 -0500

    just discovered that UserApi was returning the wrong values.
    the tests for that api are clearly insufficient.

--- src/main/scala/chatless/services/clientApi/UserApi.scala
+++ src/main/scala/chatless/services/clientApi/UserApi.scala
@@ -1,18 +1,22 @@
 package chatless.services.clientApi
 
 import spray.routing._
-
-
 import chatless._
 import chatless.services._
 import chatless.model._
-import chatless.responses.{BoolR, StringR, UserNotFoundError}
 import chatless.db.UserDAO
+import chatless.responses.UserNotFoundError
+import org.json4s._
+import org.json4s.JsonDSL._
+
 
 trait UserApi extends ServiceBase {
 
   val userDao: UserDAO
 
+  private def fieldComplete[A <% JValue](field: String)(value: A) = path(field / PathEnd) {
+    complete { Map(field -> value) }
+  }
   private def userFieldsSelector(cid: UserId, user: => User): Set[String] =
     if (user.id == cid)
       User.allFields
@@ -21,57 +25,48 @@ trait UserApi extends ServiceBase {
     else
       User.publicFields
 
-  private def followerRoutes(user: User): Route =
-    path(User.INFO / PathEnd) {
-      complete { user.info }
-    } ~ path(User.FOLLOWING / PathEnd) {
-      complete { user.following }
-    } ~ path(User.FOLLOWERS / PathEnd) {
-      complete { user.followers }
-    } ~ path(User.TOPICS / PathEnd) {
-      complete { user.topics }
-    } ~ setCompletion(
-      User.FOLLOWING -> user.following,
-      User.FOLLOWERS -> user.followers,
-      User.TOPICS -> user.topics)
-
-  private def callerRoutes(user: User): Route =
-    path(User.BLOCKED / PathEnd) {
-      complete { user.blocked }
-    } ~ path(User.TAGS / PathEnd) {
-      complete { user.tags }
-    } ~ setCompletion(
-      User.BLOCKED -> user.blocked,
-      User.TAGS -> user.tags)
-
-  private def publicRoutes(user: User)(fields: => Set[String]): Route =
-    path(PathEnd) {
-      complete { user getFields fields }
-    } ~ path(User.ID / PathEnd) {
-      complete(StringR(user.id))
-    } ~ path(User.NICK / PathEnd) {
-      complete(StringR(user.nick))
-    } ~ path(User.PUBLIC / PathEnd) {
-      complete(BoolR(user.public))
-    }
-
-  private def userGets(cid: UserId)(user: User): Route =
-    publicRoutes(user) {
-      userFieldsSelector(cid = cid, user = user)
-    } ~ authorize(check = user.public || (user.followers contains cid)) {
-      followerRoutes(user)
-    } ~ authorize(user.id == cid) {
-      callerRoutes(user)
-    }
-
 
   val userApi: CallerRoute = cid => get {
     resJson {
       pathPrefix(USER_API_BASE / Segment) { uid: UserId =>
-        userGets(cid) {
-          userDao get uid getOrElse {
-            throw UserNotFoundError(uid)
-          }
+        val user = userDao get uid getOrElse { throw UserNotFoundError(uid) }
+        /*----------------------------------------*/
+        /*these paths can be requested by any user*/
+        path(PathEnd) {
+          complete { user getFields userFieldsSelector(cid, user) }
+        } ~ fieldComplete(User.ID) {
+          user.id
+        } ~ fieldComplete(User.NICK) {
+          user.nick
+        } ~ fieldComplete(User.PUBLIC) {
+          user.public
+        } ~ authorize(check = user.public || (user.followers contains cid) || (user.id == cid)) {
+          /*---------------------------------------------------------------------------------------------*/
+          /*if the user is public, is followed by the caller, or is the caller, the caller can read these*/
+          fieldComplete(User.INFO) {
+            user.info
+          } ~ fieldComplete(User.FOLLOWING) {
+            user.following
+          } ~ fieldComplete(User.FOLLOWERS) {
+            user.followers
+          } ~ fieldComplete(User.TOPICS) {
+            user.topics
+          } ~ setCompletion(
+            User.FOLLOWING -> user.following,
+            User.FOLLOWERS -> user.followers,
+            User.TOPICS    -> user.topics)
+          /*---------------------------------------------------------------------------------------------*/
+        } ~ authorize(user.id == cid) {
+          /*---------------------------------------------*/
+          /*only the user requested can read these fields*/
+          fieldComplete(User.BLOCKED) {
+            user.blocked
+          } ~ fieldComplete(User.TAGS) {
+            user.tags
+          } ~ setCompletion(
+            User.BLOCKED -> user.blocked,
+            User.TAGS    -> user.tags)
+          /*---------------------------------------------*/
         }
       }
     }
